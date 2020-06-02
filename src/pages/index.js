9a465e379faf2af181183b33684e6ebd086d57fb
Ismael Guimarães <56137836+ismaelguimaraes@users.noreply.github.com>
2020-06-02 20:49:18 -0300

    Ajustes finos

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,10 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Footer from '../components/Footer'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Início" />
-    <Footer />
   </Layout>
 )
 

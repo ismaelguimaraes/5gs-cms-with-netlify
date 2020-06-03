aba276da091e98e5b25abcc595b861bfb993b59b
Ismael Guimarães <56137836+ismaelguimaraes@users.noreply.github.com>
2020-06-02 23:15:50 -0300

    Finalização no design do card

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'gatsby'
 
-import { CardText, CardTitle } from 'reactstrap';
-import { CardBlog, CardImgBlog, CardBodyBlog } from './styled';
+import { CardBlog, CardImgBlog, CardBodyBlog, CardTitleBlog, CardTextBlog, LinkBlog } from './styled';
 
 const CardNoticia = (props) => {
     return (
         <CardBlog>
             <CardImgBlog top width="100%" src={props.srcImagem} alt={props.title} />
             <CardBodyBlog>
-                <CardTitle>{props.title}</CardTitle>
-                <CardText>Some quick example text to build on the card title.</CardText>
-                <Link to={props.src}>Button</Link>
+                <CardTitleBlog>{props.title}</CardTitleBlog>
+                <CardTextBlog>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC</CardTextBlog>
+                <div className="d-flex justify-content-center">
+                    <LinkBlog to={props.src}>Leia mais+</LinkBlog>
+                </div>
             </CardBodyBlog>
         </CardBlog>
     )
--- src/components/Cards/styled.js
+++ src/components/Cards/styled.js
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
-import { Card, CardImg, CardBody } from 'reactstrap'
+import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap'
+import { Link } from 'gatsby'
 
 export const CardBlog = styled(Card)`
     border: 0;
+    overflow: hidden;
+
+    &:hover {
+        img {
+            transform: scale(1.01);
+            transition: all 0.4s ease;
+        }
+    }
 `
 
 export const CardImgBlog = styled(CardImg)`
     margin: 0;
+    overflow: hidden;
     border-radius: 30px 30px 0 0;
 `
 
@@ -14,4 +24,34 @@ export const CardBodyBlog = styled(CardBody)`
     background-color: #396A47;
     padding: 0.75rem;
     border-radius: 0 0 30px 30px;
+`
+
+export const CardTitleBlog = styled(CardTitle)`
+    font-family: 'Bebas Neue', sans-serif;
+    font-size: 24px;
+    color: #FFFFFF;
+`
+
+export const CardTextBlog = styled(CardText)`
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    color: #FFFFFF;
+    line-height: 18px;
+`
+
+export const LinkBlog = styled(Link)`
+    font-family: 'Roboto', sans-serif;
+    color: #FFFFFF;
+    font-size: 16px;
+    padding: 8px 16px;
+    font-weight: 600;
+    border: 1px solid #FFFFFF;
+    border-radius: 8px;
+
+    &:hover {
+        text-decoration: none;
+        color: #396A47;
+        background-color: #FFFFFF;
+        transition: all 0.4s ease;
+    }
 `
